df0b58df5ed18af4e92e61e529d0b19a3e40cea3
kyeah <39901387+dmobtxx19@users.noreply.github.com>
2023-11-25 17:14:00 +0900

    init

---
+++ src/main.ts
@@ -0,0 +1,31 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('BLURTING API')
+    .setDescription('API for BLURTING service')
+    .setVersion('1.0')
+    .build();
+
+  const docs = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api', app, docs);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+    }),
+  );
+
+  await app.listen(3000);
+}
+
+bootstrap();
